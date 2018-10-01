96d64df4f3d3553a960ebd3bc52450c074304ecb
Muhammad Mahdi Husaayni <mmhusayni2@gmail.com>
2018-10-01 23:54:59 +0800

    notification is read

--- GigHub/GigHub/Api/NotificationsController.cs
+++ GigHub/GigHub/Api/NotificationsController.cs
@@ -29,5 +29,19 @@ namespace GigHub.Api
                 .ToList();
             return notifications;
         }
+
+        [HttpPost]
+        public IHttpActionResult MarkAsRead()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId && !un.IsRead)
+                .ToList();
+
+            notifications.ForEach(n => n.Read());
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
